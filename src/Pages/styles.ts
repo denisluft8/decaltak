fbaa8936fb540a523d130aa6a593d97eae4f1f22
Dênis Augusto Luft <denisluft8@gmail.com>
2023-03-07 09:09:39 -0300

    background color change

--- src/Pages/styles.ts
+++ src/Pages/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Page = styled.section`
   align-items: center;
-  background-color: #121214;
+  background-color:${(props) => (props.color ? props.color : "#121214")};
   display: flex;
   justify-content: center;
   padding-top: 70px;
